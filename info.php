cea76a9ff252fdf18257c70e0e3c063eae9dd603
Tanish Raj <58633925+cttricks@users.noreply.github.com>
2021-06-10 19:10:52 +0530

    Updated Info file
    This is now important for all rest files

--- info.php
+++ info.php
@@ -1,7 +1,34 @@
 <?php
+/*Secured credentials*/
 $xTinyDB = array(
-	"locator" => "storage-area/bucket-locators.json",
 	"storage" => "storage-area"
+	"accessKey" => "sampleaccesskey01"
 );
+
+$xTinyDB["locator"] = $xTinyDB["storage"] ."/bucket-locators.json";
+
+/*Default response*/
+header("HTTP/1.0 404 Method not allowed");
+$cResponse = array("status"=>"Failed", "msg"=> "method not allowed");
+
+/*Check storage Area*/
+if(!is_dir($xTinyDB["storage"])){
+	mkdir($xTinyDB["storage"]);
+	/*Create Locator JSON*/
+	file_put_contents($xTinyDB["locator"], json_encode(array(), JSON_PRETTY_PRINT));
+}
+
+/*Verify Token*/
+$headers = getallheaders();
+if($xTinyDB["accessKey"] !== str_replace("Basic ","", $headers["Authorization"])){
+	$cResponse['msg'] = "invalid access key";
+	die(json_encode($cResponse));
+}
+
+/*Remove Special Chars from the Tags/Bucket Name*/
+function clean($string) {
+   $string = str_replace(' ', '_', $string); 
+   return preg_replace('/[^A-Za-z0-9\/_]/', '', $string);
+}
 
 ?>
